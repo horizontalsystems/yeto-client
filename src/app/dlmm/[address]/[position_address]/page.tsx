24f63e7f617a3ab6c845fce003a272df165949c7
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-05-02 19:07:03 +0600

    Remove unnecessary chart in position page

--- src/app/dlmm/[address]/[position_address]/page.tsx
+++ src/app/dlmm/[address]/[position_address]/page.tsx
@@ -37,27 +37,7 @@ export default async function DlmmPositionPage({
 
       <div className="mt-6 flex flex-1 flex-col gap-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-          <div className="bg-card rounded-xl p-5">
-            <div className="flex justify-between">
-              <div>
-                <div>Price Range</div>
-                <div>$926,497,170.32</div>
-              </div>
-              <div className="flex gap-2">
-                <Button size="sm">1D</Button>
-                <Button size="sm" variant="outline" className="text-white">
-                  30D
-                </Button>
-                <Button size="sm" variant="outline" className="text-white">
-                  90D
-                </Button>
-                <Button size="sm" variant="outline" className="text-white">
-                  All
-                </Button>
-              </div>
-            </div>
-            <TvlVolumeChart />
-          </div>
+          <TvlVolumeChart />
           <div className="bg-card rounded-xl p-5">
             <div className="flex gap-2">
               <ButtonClaim poolAddress={address} positionAddress={position_address} />
