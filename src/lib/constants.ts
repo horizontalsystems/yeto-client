d1b4fefdc769ea620d95843a2396d9998bc6e6cb
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-05-05 15:20:42 +0600

    Update DLMM contract address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,6 @@ export const SOL_TOKEN_MINT = 'So11111111111111111111111111111111111111112'
 export const USDC_TOKEN_MINT = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
 
 export const DLMM_PROGRAM_IDS = {
-  devnet: 'BtETEEka6hQNPnbNKPkTm66wA9BPZp18xSAAZ9AcKjgo',
-  'mainnet-beta': 'BtETEEka6hQNPnbNKPkTm66wA9BPZp18xSAAZ9AcKjgo'
+  devnet: 'G1qfD4kRvEz9jUaKqRsbGRgJtU8TiD6njQmW5JKaRTaq',
+  'mainnet-beta': 'G1qfD4kRvEz9jUaKqRsbGRgJtU8TiD6njQmW5JKaRTaq'
 }
