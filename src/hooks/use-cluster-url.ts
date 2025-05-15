80eeeb9daa15f709e2433b0de425c1a03e11d270
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-05-15 19:14:27 +0600

    Fallback to mainnet-beta RPC

--- src/hooks/use-cluster-url.ts
+++ src/hooks/use-cluster-url.ts
@@ -1,6 +1,9 @@
 import { useMemo } from 'react'
 import { clusterApiUrl } from '@solana/web3.js'
 
-export function useClusterUrl(isMainnet: boolean = true) {
-  return useMemo(() => (isMainnet ? process.env.NEXT_PUBLIC_RPC_URL : clusterApiUrl('devnet')), [isMainnet])
+export function useClusterUrl(isMainnet: boolean = true): string {
+  return useMemo(
+    () => (isMainnet ? process.env.NEXT_PUBLIC_RPC_URL || clusterApiUrl('mainnet-beta') : clusterApiUrl('devnet')),
+    [isMainnet]
+  )
 }
